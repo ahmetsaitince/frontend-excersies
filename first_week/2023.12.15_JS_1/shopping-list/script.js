8cd1394c652e91447ced78d5d2aa50cb0be4006d
Ahmet Sait İnce <ahmetsaitince@gmail.com>
2023-12-17 13:05:51 +0300

    saturday_update

--- first_week/2023.12.15_JS_1/shopping-list/script.js
+++ first_week/2023.12.15_JS_1/shopping-list/script.js
@@ -3,6 +3,31 @@ forms.forEach((form) => console.log(form));
 
 const output = document.getElementById('app-title');
 
-document.getElementById('app-title').title = 'Ahmet';
-document.getElementById('app-title').innerText = 'Ahmet Sait İnce';
-console.log(output);
+const title = document.getElementById('app-title');
+
+// Get / change content
+
+title.innerText = 'Ahmet Sait İnce';
+title.innerHTML = '<span>Murat Alemdar</span>';
+
+// Change styles
+
+title.style.backgroundColor = 'black';
+title.style.color = '#f1f1f1';
+title.style.padding = '10px';
+title.style.borderRadius = '10px';
+
+// document.querySelector()
+
+console.log(document.querySelector('input[type="text"]'));
+console.log(document.querySelector('#app-title'));
+
+console.log(document.querySelector('li:nth-child(2)').innerText);
+
+// querySelectorAll()
+
+const listItems = document.querySelectorAll('.items li');
+
+listItems.forEach((item) => {
+  item.style.color = 'red';
+});
